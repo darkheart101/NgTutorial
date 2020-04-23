19a8cf5b14f3a6c1e6bfb6942716b189e2890a86
darkheart101 <kurokokoro@windowslive.com>
2020-04-23 06:47:25 +0300

    products list

---
+++ NgTutorial/src/app/products/product-list.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'pm-products',
+    templateUrl: 'product-list.component.html'
+})
+
+export class ProductListComponent implements OnInit {
+    pageTitle: string = 'Product List'
+
+    constructor() { }
+
+    ngOnInit() { }
+}
