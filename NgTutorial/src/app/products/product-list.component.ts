fe2790a2285bccd5513dccb9df389b60da58a2d3
darkheart101 <kurokokoro@windowslive.com>
2020-04-23 07:13:56 +0300

    interpolation and ng directives

--- NgTutorial/src/app/products/product-list.component.ts
+++ NgTutorial/src/app/products/product-list.component.ts
@@ -7,6 +7,28 @@ import { Component, OnInit } from '@angular/core';
 
 export class ProductListComponent implements OnInit {
     pageTitle: string = 'Product List'
+    products: any[] = [
+        {
+            "productId": 2,
+            "productName": "Garden Cart",
+            "productCode": "GDN-0023",
+            "releaseDate": "March 18, 2019",
+            "description": "fancy description heare",
+            "price": 32.99,
+            "starRating": 4.2,
+            "imageUrl": ""
+        },
+        {
+            "productId": 3,
+            "productName": "Hammer",
+            "productCode": "TBX-0023",
+            "releaseDate": "March 22, 2019",
+            "description": "fancy description heare",
+            "price": 8.99,
+            "starRating": 4.6,
+            "imageUrl": ""
+        }
+    ];
 
     constructor() { }
 
