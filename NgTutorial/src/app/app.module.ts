fbf6ff5cca5680ae9b1fc311d1fde24f77be59bc
darkheart101 <kurokokoro@windowslive.com>
2020-04-28 06:49:18 +0300

    Building Nested Components

--- NgTutorial/src/app/app.module.ts
+++ NgTutorial/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { FormsModule } from '@angular/forms'
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces';
+import { StarCompoent } from './shared/star.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
-    ConvertToSpacesPipe
+    ConvertToSpacesPipe,
+    StarCompoent
   ],
   imports: [
     BrowserModule,
