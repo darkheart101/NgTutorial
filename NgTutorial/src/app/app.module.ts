6f75776acbb0a1d2add6200f291996d2290bbb7c
darkheart101 <kurokokoro@windowslive.com>
2020-05-04 06:43:42 +0300

    Navigation and Routing Basics

--- NgTutorial/src/app/app.module.ts
+++ NgTutorial/src/app/app.module.ts
@@ -1,25 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { FormsModule } from '@angular/forms'
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces';
 import { StarCompoent } from './shared/star.component';
-import { from } from 'rxjs';
+import { ProductDetailsComponent } from './products/product-details.component';
+import { WelcomeComponent } from './home/welcome.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     ConvertToSpacesPipe,
-    StarCompoent
+    StarCompoent,
+    ProductDetailsComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path:'products', component: ProductListComponent },
+      { path:'products/:id', component: ProductDetailsComponent },
+      { path:'welcome', component: WelcomeComponent },
+      { path:'', redirectTo:'welcome', pathMatch:'full' },
+      { path:'**', redirectTo:'welcome', pathMatch:'full' },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
